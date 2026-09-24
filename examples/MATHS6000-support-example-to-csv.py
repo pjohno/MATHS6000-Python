82a556b70d57defd8ed305bbb4c65d4ae17ee1ae
Paul Johnson - Work Laptop <p.johno@gmail.com>
2026-09-24 16:02:40 +0100

    Class 2 - Update 2026

--- examples/MATHS6000-support-example-to-csv.py
+++ examples/MATHS6000-support-example-to-csv.py
@@ -5,19 +5,18 @@ A program to output some results into a csv file
 # libraries and methods
 from math import exp,pi
 
-# open a file to output some results
-f = open("results.csv",'w')
-
 # let the user know whats happening
-print("file opened, outputing results to file")
-for i in range(0,101):
-    x = i*0.01
-    # do some calculations
-    y = pi*x*exp(-x)
-    # output to string and format if necessary
-    str = f" {x} , {y} \n"
-    f.write(str)
-f.close()
-print("file output complete, file closed")
+print("Opening file, outputing results to file")
+# open a file to output some results
+with open("results.csv", "w") as f:
+
+    for i in range(0,101):
+        x = i*0.01
+        # do some calculations
+        y = pi*x*exp(-x)
+        # output to string and format if necessary
+        str = f" {x} , {y} \n"
+        f.write(str)
+print("File output complete, file closed")
 
 
